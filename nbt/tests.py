7d289f0cc4cf91197108569ba361cff934ebaf38
tWoolie <woolford.thomas@gmail.com>
2010-08-09 02:56:40 +0930

    release 0.5 to github

---
+++ nbt/tests.py
@@ -0,0 +1,16 @@
+import random
+import unittest
+
+class TestNBTFunctions(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def readBigTest(self):
+        pass
+    
+    def writeBigTest(self):
+        pass
+    
+if __name__ == '__main__':
+    unittest.main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='NBT',
+      version='0.1',
+      description='Named Binary Tag Reader/Writer',
+      author='Thomas Woolford',
+      author_email='woolford.thomas@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      packages=['nbt',],
+     )
