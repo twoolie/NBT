b4e2883cf9a44869f5fbb63679e4efe0d13ea8b7
tWoolie <woolford.thomas@gmail.com>
2010-08-11 00:38:05 +0930

    Added more tests

--- nbt/tests.py
+++ nbt/tests.py
@@ -1,16 +1,36 @@
-import random
+from nbt import *
 import unittest
 
-class TestNBTFunctions(unittest.TestCase):
+class ReadWriteTest(unittest.TestCase):
+    # test that we can read the test file correctly
+    def readBigTest(self):
+        mynbt = NBTFile("Bigtest.py")
+        self.assertTrue(mynbt.file != None)
+    
+    # test that we can write the test file correctly
+    def writeBigTest(self):
+        mynbt = NBTFile("Bigtest.py")
+        mynbt.write_file("output file.nbt")
+        self.assertTrue(True)
+                
+class TreeManipulationTest(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.nbtfile = NBTFile()
     
-    def readBigTest(self):
-        pass
-    
-    def writeBigTest(self):
-        pass
-    
+    def testRootNodeSetup(self):
+        self.nbtfile.name = TAG_String("Hello World")
+        self.assertEqual(self.nbtfile.name.value, "Hello World")
+        
+    def testTagAdd(self):
+        self.testRootNodeSetup()
+        #try a simple byte tag
+        self.nbtfile.tags.append(TAG_Byte(name="TestByte", value=10))
+        self.assertEqual(self.nbtfile["TestByte"].value, 10)
+        #etcetera..... will finish later
+        
+    def tearDown(self):
+        del self.nbtfile
+        
 if __name__ == '__main__':
     unittest.main()
