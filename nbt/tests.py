dcecbf21801c0813bd4fcd3a5526b72cc9f0f3de
Thomas Woolford <rocker.of.beds@gmail.com>
2010-10-17 00:38:34 +1030

    Added all information needed for uplaod to pypi. tweaked tests, still only 2 run... :(

--- nbt/tests.py
+++ nbt/tests.py
@@ -1,17 +1,21 @@
 from nbt import *
 import unittest
 
-class ReadWriteTest(unittest.TestCase):
-    # test that we can read the test file correctly
+class ReadWriteTest(unittest.TestCase):     # test that we can read the test file correctly
+    def setUp(self):
+        pass
+    
     def readBigTest(self):
         mynbt = NBTFile("Bigtest.py")
         self.assertTrue(mynbt.file != None)
     
-    # test that we can write the test file correctly
     def writeBigTest(self):
         mynbt = NBTFile("Bigtest.py")
         mynbt.write_file("output file.nbt")
         self.assertTrue(True)
+    
+    def tearDown(self):
+        pass
                 
 class TreeManipulationTest(unittest.TestCase):
     
@@ -27,7 +31,8 @@ class TreeManipulationTest(unittest.TestCase):
         #try a simple byte tag
         self.nbtfile.tags.append(TAG_Byte(name="TestByte", value=10))
         self.assertEqual(self.nbtfile["TestByte"].value, 10)
-        #etcetera..... will finish later
+        
+    #etcetera..... will finish later
         
     def tearDown(self):
         del self.nbtfile
--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 from distutils.core import setup
 
-setup(name='NBT',
-      version='0.1',
+setup(
+      name='NBT',
+      version='0.5',
       description='Named Binary Tag Reader/Writer',
       author='Thomas Woolford',
       author_email='woolford.thomas@gmail.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-      packages=['nbt',],
+      url='http://github.com/twoolie/NBT',
+      license = open("LICENSE.txt").read(),
+      long_description = open("README.txt").read(),
+      packages=['nbt'],
+      package_data={'mypkg': ['*.nbt']},
      )
