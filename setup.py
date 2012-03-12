eee282a6914903e718774d1b1d21ee75e60be175
Thomas Woolford <woolford.thomas@gmail.com>
2012-03-12 15:32:33 +1030

    Added initial support for travis

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 from nbt import VERSION
+import tests
 
 setup(
-      name='NBT',
-      version=".".join(str(x) for x in VERSION),
-      description='Named Binary Tag Reader/Writer',
-      author='Thomas Woolford',
-      author_email='woolford.thomas@gmail.com',
-      url='http://github.com/twoolie/NBT',
-      license = open("LICENSE.txt").read(),
-      long_description = open("README.txt").read(),
-      packages=['nbt'],
-     )
+  name             = 'NBT',
+  version          = ".".join(str(x) for x in VERSION),
+  description      = 'Named Binary Tag Reader/Writer',
+  author           = 'Thomas Woolford',
+  author_email     = 'woolford.thomas@gmail.com',
+  url              = 'http://github.com/twoolie/NBT',
+  license          = open("LICENSE.txt").read(),
+  long_description = open("README.txt").read(),
+  packages         = ['nbt'],
+  test_suite       = 'tests',
+  use_2to3         = True
+)
