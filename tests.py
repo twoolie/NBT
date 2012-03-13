0c41a0f27c856d3f435eb5b68e20cd7fcf093013
Freek Dijkstra <freek@macfreek.nl>
2012-03-13 10:21:00 +0000

    Write-tests now work on a copy of the test file. Closes #19

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 from nbt.nbt import _TAG_Numeric, MalformedFileError, NBTFile, TAGLIST
-import unittest
-from StringIO import StringIO
+import sys,os,unittest
+import tempfile, shutil
+from io import BytesIO
 from gzip import GzipFile
-import tempfile
 
 class BugfixTest(unittest.TestCase):
     """Bugfix regression tests."""
@@ -11,7 +12,7 @@ class BugfixTest(unittest.TestCase):
         Opening an empty file causes an uncaught exception.
         https://github.com/twoolie/NBT/issues/issue/4
         """
-        temp = StringIO("")
+        temp = BytesIO("")
         temp.seek(0)
         self.assertRaises(MalformedFileError, NBTFile, buffer=temp)
 
@@ -20,27 +21,50 @@ class BugfixTest(unittest.TestCase):
         test that files opened from a file name are closed after 
         being written to. i.e. will read correctly in the future
         """
+        # copy the file (don't work on the original test file)
+        tempdir = tempfile.mkdtemp()
+        filename = os.path.join(tempdir, 'bigtest.nbt')
+        shutil.copy('bigtest.nbt', filename)
+        
         #open the file
-        f = NBTFile("bigtest.nbt")
+        f = NBTFile(filename)
         f.write_file()
         # make sure it can be read again directly after
-        f = NBTFile("bigtest.nbt")
+        f = NBTFile(filename)
+        
+        # remove the temporary file
+        try:
+            shutil.rmtree(tempdir)
+        except OSError as e:
+            raise
 
-class ReadWriteTest(unittest.TestCase):     # test that we can read the test file correctly
+class ReadWriteTest(unittest.TestCase):
+    """test that we can read the test file correctly"""
 
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+        self.filename = os.path.join(self.tempdir, 'bigtest.nbt')
+        shutil.copy('bigtest.nbt', self.filename)
+
+    def tearDown(self):
+        try:
+            shutil.rmtree(self.tempdir)
+        except OSError as e:
+            raise
+    
     def testReadBig(self):
-        mynbt = NBTFile("bigtest.nbt")
+        mynbt = NBTFile(self.filename)
         self.assertTrue(mynbt.filename != None)
         self.assertEqual(len(mynbt.tags), 11)
 
     def testWriteBig(self):
-        mynbt = NBTFile("bigtest.nbt")
-        output = StringIO()
+        mynbt = NBTFile(self.filename)
+        output = BytesIO()
         mynbt.write_file(buffer=output)
         self.assertEqual(GzipFile("bigtest.nbt").read(), output.getvalue())
 
     def testWriteback(self):
-        mynbt = NBTFile("bigtest.nbt")
+        mynbt = NBTFile(self.filename)
         mynbt.write_file()
 
 class TreeManipulationTest(unittest.TestCase):
@@ -69,14 +93,14 @@ class EmptyStringTest(unittest.TestCase):
 
     def setUp(self):
         self.golden_value = "\x0A\0\x04Test\x08\0\x0Cempty string\0\0\0"
-        self.nbtfile = NBTFile(buffer=StringIO(self.golden_value))
+        self.nbtfile = NBTFile(buffer=BytesIO(self.golden_value))
 
     def testReadEmptyString(self):
         self.assertEqual(self.nbtfile.name, "Test")
         self.assertEqual(self.nbtfile["empty string"].value, "")
 
     def testWriteEmptyString(self):
-        buffer = StringIO()
+        buffer = BytesIO()
         self.nbtfile.write_file(buffer=buffer)
         self.assertEqual(buffer.getvalue(), self.golden_value)
 
