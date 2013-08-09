739f2d09ddc259d6df003ba32d08222524b96299
Freek Dijkstra <freek@macfreek.nl>
2013-08-09 21:18:05 +0200

    Move generation and print to separate functions.

--- examples/generate_level_dat.py
+++ examples/generate_level_dat.py
@@ -19,31 +19,36 @@ except ImportError:
 	sys.path.append(extrasearchpath)
 from nbt.nbt import NBTFile, TAG_Long, TAG_Int, TAG_String, TAG_Compound
 
+def generate_level():
+	level = NBTFile() # Blank NBT
+	level.name = "Data"
+	level.tags.extend([
+		TAG_Long(name="Time", value=1),
+		TAG_Long(name="LastPlayed", value=int(time.time())),
+		TAG_Int(name="SpawnX", value=0),
+		TAG_Int(name="SpawnY", value=2),
+		TAG_Int(name="SpawnZ", value=0),
+		TAG_Long(name="SizeOnDisk", value=0),
+		TAG_Long(name="RandomSeed", value=random.randrange(1,9999999999)),
+		TAG_Int(name="version", value=19132),
+		TAG_String(name="LevelName", value="Testing")
+	])
+	
+	player = TAG_Compound()
+	player.name = "Player"
+	player.tags.extend([
+		TAG_Int(name="Score", value=0),
+		TAG_Int(name="Dimension", value=0)
+	])
+	inventory = TAG_Compound()
+	inventory.name = "Inventory"
+	player.tags.append(inventory)
+	level.tags.append(player)
+	
+	return level
 
-level = NBTFile() # Blank NBT
-level.name = "Data"
-level.tags.extend([
-	TAG_Long(name="Time", value=1),
-	TAG_Long(name="LastPlayed", value=int(time.time())),
-	TAG_Int(name="SpawnX", value=0),
-	TAG_Int(name="SpawnY", value=2),
-	TAG_Int(name="SpawnZ", value=0),
-	TAG_Long(name="SizeOnDisk", value=0),
-	TAG_Long(name="RandomSeed", value=random.randrange(1,9999999999)),
-	TAG_Int(name="version", value=19132),
-	TAG_String(name="LevelName", value="Testing")
-])
 
-player = TAG_Compound()
-player.name = "Player"
-player.tags.extend([
-	TAG_Int(name="Score", value=0),
-	TAG_Int(name="Dimension", value=0)
-])
-inventory = TAG_Compound()
-inventory.name = "Inventory"
-player.tags.append(inventory)
-level.tags.append(player)
-
-print(level.pretty_tree())
-#level.write_file("level.dat")
+if __name__ == '__main__':
+	level = generate_level()
+	print(level.pretty_tree())
+	#level.write_file("level.dat")
