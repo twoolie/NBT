twoolie/NBT

commit 9b8b434b32eefd231d8d116b06b48e8c41ae0eba

Rewrite locate_free_space(). Bug fixes, in particular when chunks overlap.

nbt/region.py
@@ -288,7 +288,7 @@ class RegionFile(object):
 					# There are not enough sectors allocated for the whole block
 					m.status = self.STATUS_CHUNK_MISMATCHED_LENGTHS
 
-	def _sectors(self):
+	def _sectors(self, ignore_chunk=None):
 		"""
 		Return a list of all sectors, each sector is a list of chunks occupying the block.
 		"""
@@ -299,14 +299,29 @@ class RegionFile(object):
 		for m in self._header.values():
 			if not m.is_created():
 				continue
+			if ignore_chunk == m:
+				continue
 			if m.blocklength and m.blockstart:
 				for b in range(m.blockstart, m.blockstart + m.blocklength):
 					if 2 <= b < sectorsize:
 						sectors[b].append(m)
 		return sectors
 
-	def locate_free_space(self, required_sectors):
-		pass
+	def locate_free_space(self, required_sectors=1, ignore_chunk=None, preferred=0):
+		sectors = self._sectors(ignore_chunk=ignore_chunk)
+		free_locations = [i != None and len(i) == 0 for i in sectors] + required_sectors*[True]
+		
+		# check preferred (current) location
+		if all(free_locations[preferred:preferred+required_sectors]):
+			return preferred
+		
+		# check other locations
+		i = 2
+		while i < len(sectors):
+			if all(free_locations[i:i+required_sectors]):
+				break
+			i += 1
+		return i
 
 	def get_chunk_metadata(self):
 		"""
@@ -431,9 +446,9 @@ class RegionFile(object):
 	def write_chunk(self, x, z, nbt_file):
 		""" A simple chunk writer. """
 		data = BytesIO()
-		nbt_file.write_file(buffer = data) #render to buffer; uncompressed
+		nbt_file.write_file(buffer = data) # render to buffer; uncompressed
 
-		compressed = zlib.compress(data.getvalue()) #use zlib compression, rather than Gzip
+		compressed = zlib.compress(data.getvalue()) # use zlib compression, rather than Gzip
 		data = BytesIO(compressed)
 
 		# 5 extra bytes are required for the chunk block header
@@ -443,62 +458,10 @@ class RegionFile(object):
 			raise ChunkDataError("Chunk is too large (%d sectors exceeds 255 maximum)" % (nsectors))
 
 		# search for a place where to write the chunk:
-		offset, length, timestamp, status = self.header[x, z]
+		current = self._header[x, z]
+		sector = self.locate_free_space(nsectors, ignore_chunk = current, preferred = current.blockstart)
+		
 		pad_end = False
-
-		if status in (self.STATUS_CHUNK_NOT_CREATED, self.STATUS_CHUNK_OK):
-			# look up if the new chunk fits in the place of the old one,
-			# a no created chunk has 0 length, so can't be a problem
-			if nsectors <= length:
-				sector = offset
-			else:
-				# let's find a free place for this chunk
-				found = False
-				# sort the chunk tuples by offset and ignore empty chunks
-				l = sorted([i for i in self.header.values() if i[0] != 0])
-
-				# TODO: What is l[0][0]?
-				if l[0][0] != 2:
-					# there is space between the header and the first
-					# used sector, insert a false tuple to check that
-					# space too
-					l.insert(0,(2,0,0,0))
-
-				# iterate chunks by offset and search free space
-				for i in range(len(l) - 1):
-					# first item in the tuple is offset, second length
-					
-					current_chunk = l[i]
-					next_chunk = l[i+1]
-					# calculate free_space beween chunks and break if enough
-					free_space = next_chunk[0] - (current_chunk[0] + current_chunk[1])
-					if free_space >= nsectors:
-						sector = current_chunk[0] + current_chunk[1]
-						# a corrupted region header can contain random
-						# stuff, just in case check if we are trying to
-						# write in the header and skip if it's the case.
-						if sector <= 1:
-							continue
-						found  = True
-						break
-
-				if not found: # append chunk to the end of the file
-					self.file.seek(0, SEEK_END) # go to the end of the file
-					file_length = self.file.tell()-1 # current offset is file length
-					total_sectors = self._bytes_to_sector(file_length)
-					sector = total_sectors+1
-					pad_end = True
-		else:
-			# status is (self.STATUS_CHUNK_OUT_OF_FILE, self.STATUS_CHUNK_IN_HEADER,
-			#      self.STATUS_CHUNK_ZERO_LENGTH, self.STATUS_CHUNK_MISMATCHED_LENGTHS)
-			# don't trust bad headers, this chunk hasn't been generated yet, or the header is wrong
-			# This chunk should just be appended to the end of the file
-			self.file.seek(0, SEEK_END) # go to the end of the file
-			file_length = self.file.tell()-1 # current offset is file length
-			total_sectors = self._bytes_to_sector(file_length)
-			sector = total_sectors+1
-			pad_end = True
-
 
 		# write out chunk to region
 		self.file.seek(sector*4096)
@@ -532,7 +495,9 @@ class RegionFile(object):
 		Minecraft and this nbt library write chunks in old free spaces
 		when possible.
 		"""
-		# TODO: this function fails for an empty file. If that is the case, just return.
+		# This function fails for an empty file. If that is the case, just return.
+		if self.size < 8192:
+			return
 
 		# zero the region header for the chunk (offset length and time)
 		self.file.seek(4*(x+z*32))
@@ -540,9 +505,12 @@ class RegionFile(object):
 		self.file.seek(4096+4*(x+z*32))
 		self.file.write(pack(">I", 0))
 
+		# TODO: zero cleared chunks, provided that they are in the file and non-overlapping.
+		
+		# TODO: truncate file if possible.
+		
 		# update the header
-		self.parse_header()
-		self.parse_chunk_headers()
+		self._header[x, z] = _ChunkMetadata(x, z)
 
 	def _classname(self):
 		"""Return the fully qualified class name."""

tests/regiontests.py
@@ -482,8 +482,6 @@ class ReadWriteTest(unittest.TestCase):
 		self.assertEqual(header[3], RegionFile.STATUS_CHUNK_NOT_CREATED)
 		self.assertEqual(self.region.chunk_count(), chunk_count - 1)
 
-	# TODO: bug in free sector calculation for overlapping chunks
-	@unittest.expectedFailure
 	def test44UseEmptyChunks(self):
 		"""
 		write 1 sector chunk 1,2 (should go to 004)
@@ -591,18 +589,11 @@ class ReadWriteTest(unittest.TestCase):
 		# self.assertEqual(locations, [4, 10, 11, 18, 26])
 		# self.assertEqual(self.region.get_size(), 27*4096)
 
-	# TODO: increased chunk should remain in place, if possible
-	@unittest.expectedFailure
 	def test60WriteExistingChunkIncreaseSectorSameLocation(self):
 		"""
-		write 2 sector chunk 1,2 (should go to 010-011)
 		write 2 sector chunk 7,0 (should go to 003-004) (increase chunk size)
 		"""
 		nbt = generate_compressed_level(minsize = 5000, maxsize = 7000)
-		self.region.write_chunk(1, 2, nbt)
-		header = self.region.header[1, 2]
-		self.assertEqual(header[1], 2)
-		self.assertEqual(header[0], 10)
 		self.region.write_chunk(7, 0, nbt)
 		header = self.region.header[7, 0]
 		self.assertEqual(header[1], 2, "Chunk length must be 2 sectors")
@@ -610,30 +601,42 @@ class ReadWriteTest(unittest.TestCase):
 		self.assertEqual(header[3], RegionFile.STATUS_CHUNK_OK)
 		# self.assertEqual(self.region.get_size(), 27*4096)
 
-	def test61WriteExistingChunkIncreaseSectorNewLocation(self):
+	def test61WriteExistingChunkCorrectSize(self):
 		"""
-		write 2 sector chunk 8,0 (should go to 010-011) (increase chunk size, move to different location)
+		write 2 sector chunk 3,1 (should go to 025-026) (increase sector size)
+		"""
+		nbt = self.region.get_chunk(3, 1)
+		self.region.write_chunk(3, 1, nbt)
+		header = self.region.header[3, 1]
+		self.assertEqual(header[1], 2, "Chunk length must be 2 sectors")
+		self.assertEqual(header[0], 25, "Chunk should remain in sector 3")
+		self.assertEqual(header[3], RegionFile.STATUS_CHUNK_OK)
+		# self.assertEqual(self.region.get_size(), 27*4096)
+
+	def test62WriteExistingChunkIncreaseSectorNewLocation(self):
+		"""
+		write 2 sector chunk 8,0 (should go to 004-005 or 010-011)
 		verify chunk_count remains 18
-		should free sector 005
-		write 2 sector chunk 2,2 (should go to 004-005)
+		write 2 sector chunk 2,2 (should go to 010-011 or 004-005)
 		verify that file size is not increased <= 027*4096
 		verify chunk_count is 19
 		"""
+		locations = []
 		chunk_count = self.region.chunk_count()
 		nbt = generate_compressed_level(minsize = 5000, maxsize = 7000)
 		self.region.write_chunk(8, 0, nbt)
 		header = self.region.header[8, 0]
 		self.assertEqual(header[1], 2) # length
-		self.assertEqual(header[0], 10) # location
+		locations.append(header[0]) # location
 		self.assertEqual(self.region.chunk_count(), chunk_count)
-		# Section 005 should be free now.
 		self.region.write_chunk(2, 2, nbt)
 		header = self.region.header[2, 2]
 		self.assertEqual(header[1], 2) # length
-		self.assertEqual(header[0], 4) # location
+		locations.append(header[0]) # location
+		self.assertEqual(sorted(locations), [4, 10]) # locations
 		self.assertEqual(self.region.chunk_count(), chunk_count + 1)
 
-	def test62WriteNewChunkFreedSectors(self):
+	def test63WriteNewChunkFreedSectors(self):
 		"""
 		unlink chunk 6,0
 		unlink chunk 7,0
@@ -647,7 +650,6 @@ class ReadWriteTest(unittest.TestCase):
 		self.assertEqual(header[1], 3, "Chunk length must be 3 sectors")
 		self.assertEqual(header[0], 2, "Chunk should be placed in sector 2")
 
-	@unittest.expectedFailure
 	def test70WriteOutOfFileChunk(self):
 		"""
 		write 1 sector chunk 13,0 (should go to 004)
@@ -670,8 +672,6 @@ class ReadWriteTest(unittest.TestCase):
 		self.assertEqual(header[1], 1) # length
 		self.assertNotEqual(header[0], 19, "Previously 0-length chunk should not overwrite existing chunk")
 
-	# TODO: Chunk should not be written to same location when it overlaps
-	@unittest.expectedFailure
 	def test72WriteOverlappingChunkLong(self):
 		"""
 		write 2 sector chunk 4,0 (should go to 010-011) (free 014 & 016)
@@ -707,8 +707,6 @@ class ReadWriteTest(unittest.TestCase):
 		# self.assertEqual(locations, [4, 14, 16, 18, 26])
 		# self.assertEqual(self.region.get_size(), 27*4096)
 
-	# TODO: Chunk should not be written to same location when it overlaps
-	@unittest.expectedFailure
 	def test73WriteOverlappingChunkSmall(self):
 		"""
 		write 1 sector chunk 12,0 (should go to 004) ("free" 015 for use by 4,0)
@@ -733,8 +731,6 @@ class ReadWriteTest(unittest.TestCase):
 		# self.assertEqual(locations, [10, 11, 26])
 		# self.assertEqual(self.region.get_size(), 27*4096)
 
-	# TODO: Chunk should not be written to same location when it overlaps
-	@unittest.expectedFailure
 	def test74WriteOverlappingChunkSameLocation(self):
 		"""
 		write 1 sector chunk 12,0 (should go to 004) ("free" 012 for use by 4,0)
@@ -750,7 +746,7 @@ class ReadWriteTest(unittest.TestCase):
 		self.region.write_chunk(4, 0, nbt)
 		header = self.region.header[4, 0]
 		self.assertEqual(header[1], 3) # length
-		self.assertNotEqual(header[0], 14, "No longer overlapping chunks should be written to same location when when possible")
+		self.assertEqual(header[0], 14, "No longer overlapping chunks should be written to same location when when possible")
 
 	# TODO: bug in free sector calculation for overlapping chunks (precondition fails)
 	@unittest.expectedFailure
@@ -945,6 +941,9 @@ class EmptyFileTest(unittest.TestCase):
 		region.write_chunk(0, 0, chunk)
 		self.assertEqual(self.region.get_size(), 3*4096)
 		self.assertEqual(self.region.chunk_count(), 1)
+
+# TODO: rewrite 3,1 smaller. -> truncate file
+# TODO: rewrite 3,1 same size. -> length is 2
 
 # TODO: test suite to test the different __init__ parameters of RegionFile
 # (filename or fileobj), write to it, delete RegionFile object, and test if:

commit 59c9ed2204e58528bdb0a32d5fa7754a7f4c3316

Fix: increase file to full 4096-byte sector size. Bug introduced in 9b8b434b32. Append zeroes to the last written sector. Note: does not yet zero freed sectors.

nbt/region.py
@@ -457,9 +457,10 @@ class RegionFile(object):
 
 		compressed = zlib.compress(data.getvalue()) # use zlib compression, rather than Gzip
 		data = BytesIO(compressed)
+		length = len(data.getvalue())
 
 		# 5 extra bytes are required for the chunk block header
-		nsectors = self._bytes_to_sector(len(data.getvalue()) + 5)
+		nsectors = self._bytes_to_sector(length + 5)
 
 		if nsectors >= 256:
 			raise ChunkDataError("Chunk is too large (%d sectors exceeds 255 maximum)" % (nsectors))
@@ -467,19 +468,16 @@ class RegionFile(object):
 		# search for a place where to write the chunk:
 		current = self._header[x, z]
 		sector = self.locate_free_space(nsectors, ignore_chunk = current, preferred = current.blockstart)
-		
-		pad_end = False
 
 		# write out chunk to region
 		self.file.seek(sector*4096)
-		self.file.write(pack(">I", len(data.getvalue())+1)) #length field
+		self.file.write(pack(">I", length + 1)) #length field
 		self.file.write(pack(">B", RegionFile.COMPRESSION_ZLIB)) #compression field
 		self.file.write(data.getvalue()) #compressed data
-		if pad_end:
-			# Write zeros up to the end of the chunk
-			self.file.seek((sector+nsectors)*4096-1)
-			# TODO: this would write only one zero-byte, shouldn't this be more bytes long?
-			self.file.write(b"\x00")
+
+		# Write zeros up to the end of the chunk
+		remaininglength = 4096 * nsectors - length - 5
+		self.file.write(remaininglength * b"\x00")
 
 		#seek to header record and write offset and length records
 		self.file.seek(4*(x+z*32))
@@ -490,6 +488,10 @@ class RegionFile(object):
 		timestamp = int(time.time())
 		self.file.write(pack(">I", timestamp))
 
+		# TODO: zero cleared chunks, provided that they are in the file and non-overlapping.
+		
+		# TODO: truncate file if possible.
+		
 		# update file size and header information
 		self.parse_header()
 		self.parse_chunk_headers()

tests/regiontests.py
@@ -541,8 +541,6 @@ class ReadWriteTest(unittest.TestCase):
 		self.assertEqual(header[0], 10, "Chunk should be placed in sector 10")
 		self.assertEqual(header[3], RegionFile.STATUS_CHUNK_OK)
 
-	# TODO: file increasement should be supported
-	@unittest.expectedFailure
 	def test53WriteNewChunkIncreaseFile(self):
 		"""
 		write 3 sector chunk 2,2 (should go to 026-028) (increase file size)
@@ -953,8 +951,10 @@ class EmptyFileTest(unittest.TestCase):
 # Also test if an exception is raised if RegionFile is called incorrectly (e.g. both filename and fileobj are specified, or none)
 
 # TODO: test what happens with a corrupt region file, of 5000 bytes in size. Read a chunk, write a chunk
+
 # TODO: test what happens if a file is trucated, but the chunk is still readable. This should return the chunk.
 #       also test writing; does the file get padded?
+#       also test writing another chunk. What happens is seek() jumps to a point larger than file size? Are bytes zeroed. Should we do that manually?
 
 if __name__ == '__main__':
 	logger = logging.getLogger("nbt.tests.regiontests")