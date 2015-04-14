3e501d123267e69d571f5f6c3619c31a3c498d59
Damien Gaignon <damien.gaignon@gmail.com>
2015-04-14 18:44:22 +0200

    Fix "ValueError: byte must be in range(0, 256)"

--- examples/anvil_blockdata.py
+++ examples/anvil_blockdata.py
@@ -60,7 +60,7 @@ def grouper(iterable, n, fillvalue=None):
     return zip_longest(*args, fillvalue=fillvalue)
 
 def print_chunklayer(blocks, data, add, yoffset):
-    blocks = blocks[yoffset*256:(yoffset+1)*256]
+    blocks = list(blocks[yoffset*256:(yoffset+1)*256])
     data = array_4bit_to_byte(data[yoffset*128:(yoffset+1)*128])
     if add is not None:
         add = array_4bit_to_byte(add[yoffset*128:(yoffset+1)*128])
