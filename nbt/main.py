c9a669b290f72d72fa7289b2a259cc94c389d068
FarexDev <31214887+FarexDev@users.noreply.github.com>
2018-04-28 20:26:09 -0400

    Add files via upload

---
+++ nbt/main.py
@@ -0,0 +1,38 @@
+import sys
+import os
+
+nbt_path = os.path.dirname(__file__) + "\\NBT-1.4.1\\NBT"
+sys.path.append(nbt_path)
+import nbt
+
+class Schematic(nbt.NBTFile):
+    def __init__(self,*args,buffer="rb",**kwargs):
+        super().__init__(*args,**kwargs)
+
+        self.blocks = list(self.get_value("Blocks"))
+        self.width = self.get_value("Width")
+        self.height = self.get_value("Height")
+        self.length = self.get_value("Length")
+
+    def count(self,id_):
+        """Returns how many blocks with the specified ID are in the schematic"""
+        return self.blocks.count(id_)
+
+    def get_noair(self):
+        """Returns a list of the blocks without the air blocks"""
+        return [b for b in self.blocks if b != 0]
+    
+    def get_value(self,key):
+        """Returns the value of a given key"""
+        return self[key].value
+
+    def get_valuestr(self,key):
+        """Returns the value of a given key in string form"""
+        return self[key].valuestr()
+
+    def print_tree(self):
+        """Outputs a pretty tree to the console"""
+        print(self.pretty_tree())
+
+if __name__ == "__main__":
+    f = Schematic("Nova.schematic")
